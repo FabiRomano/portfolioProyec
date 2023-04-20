11dd773ee87afdbef01c9b15353acdd7d304fdbe
FabiRomano <99614961+FabiRomano@users.noreply.github.com>
2023-04-20 18:22:01 -0300

    footer+css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    FooterComponent,
     
   ],
 
