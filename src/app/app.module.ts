442eb7fa04e1fe3ffa19cecbf75d9a803e424176
FabiRomano <99614961+FabiRomano@users.noreply.github.com>
2023-06-22 16:05:08 -0300

    componentes nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
+import { PagComponent } from './components/pag/pag.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
+    PagComponent,
+    SobreMiComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    SkillsComponent,
     
   ],
 
